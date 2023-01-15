001ceaef2feceee4986b232b75deb09705b39a65
Sebastian Fischer <sebf.fischer@gmail.com>
2023-01-15 21:49:56 +0100

    ...

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -3,11 +3,8 @@ return {
 	cmd = "Copilot",
 	event = "InsertEnter",
 	opts = {
+		-- this has to be set to true for the plugin to work (see README.md of copilot.lua)
 		suggestion = { enabled = false },
 		panel = { enabled = false },
-		filetypes = {
-			r = true,
-			lua = true,
-		},
 	},
 }
--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 My personal dotfiles and setup notes.
 
-To create the symlinks, stow is used.
+To create the symlinks, run the following command:
 
-
+```{sh}
+make stow
+```
